066b3ae1f99339f9877285ebbaf07201df194cf2
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-06-17 18:59:35 +0200

    Use  in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -2,30 +2,30 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_NAME', $_ENV['ARTIFAKT_MYSQL_DATABASE_NAME'] );
 
 /** MySQL database username */
-define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_USER', $_ENV['ARTIFAKT_MYSQL_USER'] );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_PASSWORD', $_ENV['ARTIFAKT_MYSQL_PASSWORD'] );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('DB_HOST') );
+define( 'DB_HOST', $_ENV['ARTIFAKT_MYSQL_HOST'] );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-define( 'AUTH_KEY',         getenv('AUTH_KEY') );
-define( 'SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY') );
-define( 'LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY') );
-define( 'NONCE_KEY',        getenv('NONCE_KEY') );
-define( 'AUTH_SALT',        getenv('AUTH_SALT') );
-define( 'SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT') );
-define( 'LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT') );
-define( 'NONCE_SALT',       getenv('NONCE_SALT') );
+define('AUTH_KEY',         ($_ENV['AUTH_KEY'] != '') ?          $_ENV['AUTH_KEY']:'!0cYeO;9o]z}8R@Xp55eKiNU~ZtT[wsjW-(Ji=ddL$Yo*O`EXt82 r=EO1-[X}w(');
+define('SECURE_AUTH_KEY',  ($_ENV['SECURE_AUTH_KEY'] != '') ?   $_ENV['SECURE_AUTH_KEY']:'2/RGYVWR!:;z2tsuE=e84<Z-Oj>Uvm*GLzNz<53 @|gwBK$Mna%Jb8G#9D/A/xcS');
+define('LOGGED_IN_KEY',    ($_ENV['LOGGED_IN_KEY'] != '') ?     $_ENV['LOGGED_IN_KEY']:'T3C>W`XoF<@_+[P1P-DIh(EF=Otn;hiF D%6@gaI<`4?B`U1jH7]}&@bes]5&],{');
+define('NONCE_KEY',        ($_ENV['NONCE_KEY'] != '') ?         $_ENV['NONCE_KEY']:'*hl^lPLoe<m.)w0=Nk-J3z!InR?%LD|?$ou30I0-@,{AW>|V&R-knzDR0hX+os5X');
+define('AUTH_SALT',        ($_ENV['AUTH_SALT'] != '') ?         $_ENV['AUTH_SALT']:')*+-%6ksW-}~|nYeYf%X1uRWv4}O&m}r>P8Vp3V5A&8ly94qouQK2Nre/`_A>~Hd');
+define('SECURE_AUTH_SALT', ($_ENV['SECURE_AUTH_SALT'] != '') ?  $_ENV['SECURE_AUTH_SALT']:',ZoFK~-:L6x(LWvI01e6>S~8zhADT(qO--(aaJ)[ aZke(&i+81K&c89%M+WjFVL');
+define('LOGGED_IN_SALT',   ($_ENV['LOGGED_IN_SALT'] != '') ?    $_ENV['LOGGED_IN_SALT']:'-2,><jcgjGm!FriM7A-$6y[R@aj.f,t1)B<$`+q`+hztch~?Ka-n%M;SS|#AbLE*');
+define('NONCE_SALT',       ($_ENV['NONCE_SALT'] != '') ?        $_ENV['NONCE_SALT']:'E-;VUw{6?v tah]~^,dBmN`G=>+L:`OV# I:&h;]YM(lBNEr):BR3h>TP|Z@5=IU');
 
 $table_prefix = 'wp_';
 
