3c653aab7d2f1d4a94ffb36b1b9c7690a34c6b4f
Barracuda <barracuda@artifakt.io>
2021-06-18 09:47:18 +0200

    feature/57_env_vars: Tests

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -8,6 +8,6 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
-echo "SetEnv HTTPS off" > /etc/apache2/conf-enabled/force-ssl.conf
+echo "" > /etc/apache2/conf-enabled/force-ssl.conf
 
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
